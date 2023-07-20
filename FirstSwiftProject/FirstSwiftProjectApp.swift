4d043f0c097f0682afc48ed477899138fce51db6
Hind Suleiman Hussein <hind.suleimanh@gmail.com>
2023-07-20 19:33:39 +0300

    Initial Commit

---
+++ FirstSwiftProject/FirstSwiftProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftProjectApp.swift
+//  FirstSwiftProject
+//
+//  Created by Foothill on 20/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
