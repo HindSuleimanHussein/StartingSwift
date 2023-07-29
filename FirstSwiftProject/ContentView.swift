32a8f3638b5caf521655c5fb7cc5cbeec6ce8c8c
Hind Suleiman Hussein <hind.suleimanh@gmail.com>
2023-07-29 22:45:33 +0300

    I've updated on some stuff

--- FirstSwiftProject/ContentView.swift
+++ FirstSwiftProject/ContentView.swift
@@ -5,21 +5,34 @@
 //  Created by Foothill on 20/07/2023.
 //
 
+/*
+ commenting here
+ */
+
 import SwiftUI
 
+// MARK: -This is for view
 struct ContentView: View {
     var body: some View {
+        // I'm doing this for vertical stack of all my objects
         VStack {
+            Button("Tap on me") {
+                print("Hello");
+            }
+            
+            
+            // MARK: -Hold down option while hovering over object in order to get help faster
             Image("monstera-deliciosa")
                 .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(height: 150)
-                .cornerRadius(20)
+                .frame(width: 300, height: 400)
+                .cornerRadius(30)
             Text("This is a Plant").bold()
+                .padding(.top, 6.0)
         }
         .padding()
     }
 }
+// MARK: -This is for previous
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
